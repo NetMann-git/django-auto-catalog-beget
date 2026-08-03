e56ffe1bf8f5779c6efbca954f796ea88fcbd6e4
NetMann-git <netmann@bk.ru>
2026-08-03 14:02:45 +0300

    обавлен класс для управления кэшем каталога

---
+++ apps/products/cache.py
@@ -0,0 +1,13 @@
+# apps/products/cache.py
+
+from django.core.cache import cache
+
+
+class CatalogCache:
+    """
+    Работа с кэшем каталога.
+
+    Пока класс пустой.
+    Методы будем добавлять постепенно.
+    """
+    pass
