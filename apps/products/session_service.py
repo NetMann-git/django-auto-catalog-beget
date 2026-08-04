270d30980cd45021d5071f31894abceb0dcb9544
NetMann-git <netmann@bk.ru>
2026-08-04 22:28:13 +0300

    добавлен сервис для работы с пользовательской сессией

---
+++ apps/products/session_service.py
@@ -0,0 +1,16 @@
+
+
+"""
+Работа с данными каталога, хранящимися в пользовательской сессии.
+"""
+
+COMPARISON_KEY = "comparison"
+RECENTLY_VIEWED_KEY = "recently_viewed"
+
+
+class SessionService:
+    """
+    Сервис для работы с пользовательской сессией.
+    """
+
+    pass
