cc4fb6f3a44746fe4ec3c2bb75b133dd9627af01
NetMann-git <netmann@bk.ru>
2026-08-13 12:19:42 +0300

    добавлены миниатюры товаров в список продвигаемых рекомендаций в админке

--- apps/recommendations/admin.py
+++ apps/recommendations/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from easy_thumbnails.files import get_thumbnailer
 from .models import PromotedProduct
 
 @admin.register(PromotedProduct)
 class PromotedProductAdmin(admin.ModelAdmin):
-    list_display = ['product', 'page', 'priority', 'is_active', 'start_date', 'end_date']
+    list_display = ['image_tag', 'product', 'page', 'priority', 'is_active', 'start_date', 'end_date']
     list_editable = ['priority', 'is_active']
     list_filter = ['page', 'is_active']
     search_fields = ['product__title']
-    autocomplete_fields = ['product']  # удобный поиск товара по названию
+    autocomplete_fields = ['product']
+
+    @admin.display(description='Фото')
+    def image_tag(self, obj):
+        if obj.product.image:
+            thumbnail = get_thumbnailer(obj.product.image).get_thumbnail({
+                'size': (50, 75),
+                'crop': True,
+            })
+            return format_html('<img src="{}" style="width:50px; height:75px; object-fit: cover;" />', thumbnail.url)
+        return format_html('<span style="color: #aaa;">Нет фото</span>')
