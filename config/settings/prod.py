195909ab073affeffb2bd6df9def8feecdd3f742
NetMann-git <netmann@bk.ru>
2026-08-29 00:06:12 +0300

    обновление prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,4 +3,20 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['link23yu.beget.tech']
+# Разрешённые хосты (берём из переменной окружения, разделённые запятыми)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+
+# Настройки базы данных MySQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'link23yu_django',
+        'USER': 'link23yu_django',
+        'PASSWORD': 'J7777777d',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
